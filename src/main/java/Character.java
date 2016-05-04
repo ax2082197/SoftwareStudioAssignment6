a9ac1f42529a9ace77df85eeba882b7248c72d3c
ax2082197 <ax2082197@gmail.com>
2016-05-05 01:23:26 +0800

    test

--- src/main/java/Character.java
+++ src/main/java/Character.java
@@ -1,5 +1,10 @@
 package main.java;
 
+import java.awt.Color;
+import java.util.ArrayList;
+import java.util.HashMap;
+
+
 import processing.core.PApplet;
 
 /**
@@ -9,15 +14,48 @@ import processing.core.PApplet;
 public class Character {
 	
 	private MainApplet parent;
+	private String name;
+	private int radius = 20;	//37 character in total
+	private int iniX, iniY;
+	public float x, y;
+	private Color color;
+	private HashMap<Character,Integer> interact = new HashMap<Character,Integer>();
+	private ArrayList<Character> target = new ArrayList<Character>();
 
-	public Character(MainApplet parent){
+	public Character(MainApplet parent, String name, String color, int seq){
 
 		this.parent = parent;
-		
+		this.name = name;
+		this.color = hex2Rgb(color.substring(1));
+		iniX = (radius+20)+(seq/10)*(2*radius+20);
+		iniY = 25+(seq%10)*(2*radius+20);
+		x = iniX;
+		y = iniY;
 	}
 
 	public void display(){
-
+		parent.fill(this.color.getRGB());
+		parent.ellipse(this.x, this.y, radius, radius);
 	}
 	
+	public static Color hex2Rgb(String colorStr) {
+		//"# FF_808_080"
+	    return new Color(
+	            Integer.valueOf( colorStr.substring( 1, 3 ), 16 ),
+	            Integer.valueOf( colorStr.substring( 3, 6 ), 16 ),
+	            Integer.valueOf( colorStr.substring( 6, 9 ), 16 ) );
+	}
+	
+	public void addTarget(Character ch, int level){
+		target.add(ch);
+		interact.put(ch,level);
+	}
+	
+	public ArrayList<Character> getTarget(){
+		return target;
+	}
+	
+	public HashMap<Character,Integer> getInteract(){
+		return interact;
+	}
 }
--- src/main/java/MainApplet.java
+++ src/main/java/MainApplet.java
@@ -1,6 +1,13 @@
 package main.java;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import processing.core.PApplet;
+import processing.data.JSONArray;
+import processing.data.JSONObject;
 
 /**
 * This class is for sketching outcome using Processing
@@ -8,10 +15,22 @@ import processing.core.PApplet;
 */
 @SuppressWarnings("serial")
 public class MainApplet extends PApplet{
+	
+	private ArrayList<Character> outputList = new ArrayList<Character>();
 	private String path = "main/resources/";
-	private String file = "starwars-episode-1-interactions.json";
+	private String[] file = new String[]{
+			"starwars-episode-1-interactions.json",
+			"starwars-episode-2-interactions.json",
+			"starwars-episode-3-interactions.json",
+			"starwars-episode-4-interactions.json",
+			"starwars-episode-5-interactions.json",
+			"starwars-episode-6-interactions.json",
+			"starwars-episode-7-interactions.json"
+	};
 	
 	private final static int width = 1200, height = 650;
+	Map characters = new < Integer, ArrayList<Character>> HashMap(); //< episode ,character list>
+	
 	
 	public void setup() {
 
@@ -22,11 +41,31 @@ public class MainApplet extends PApplet{
 	}
 
 	public void draw() {
-
+		for(int i=0; i<7; i++){
+			chList
+		}
 	}
 
 	private void loadData(){
-
+		for(int j=0; j<7; j++){
+			JSONObject data = loadJSONObject(path.concat(file[j]));
+			JSONArray nodes = data.getJSONArray("nodes");
+			JSONArray links = data.getJSONArray("links");
+			List chList = new ArrayList();
+			for(int i = 0; i<nodes.size(); i++){
+				JSONObject node = nodes.getJSONObject(i);
+				Character ch = new Character( this, node.getString("name"), node.getString("colour"), i);
+				chList.add(ch);
+			}
+			characters.put(	j+1, chList);
+		
+			for(int i = 0; i<links.size(); i++){
+				JSONObject link = links.getJSONObject(i);
+				Character s =(Character) chList.get(link.getInt("source"));
+				Character t =(Character) chList.get(link.getInt("target"));
+				s.addTarget(t, link.getInt("value"));
+			}
+		}
 	}
 
 }
